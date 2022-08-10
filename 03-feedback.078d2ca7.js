fcc3b0b7b049dbc9aa4fe41f2330123417f21fe2
elenashy <97391479+elenashy@users.noreply.github.com>
2022-08-10 20:28:56 +0000

    Deploying to gh-pages from @ elenashy/goit-js-hw-08@8951ce4f0ec9c2cbd50f29f5431a8289a0039421 🚀

---
+++ 03-feedback.078d2ca7.js
@@ -0,0 +1,2 @@
+!function(){var e={formEl:document.querySelector(".feedback-form"),textareaEl:document.querySelector(".message")};e.formEl.addEventListener("submit",(function(e){})),e.textareaEl.addEventListener("input",(function(e){}))}();
+//# sourceMappingURL=03-feedback.078d2ca7.js.map
