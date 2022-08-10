9ec2a3ea8e93659c9d0bb813b4ad5086483ef22a
elenashy <97391479+elenashy@users.noreply.github.com>
2022-08-10 22:00:19 +0300

    start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const LOCALE_STORAGE_KEY = 'videoplayer-current-time'
+
+const onPlay = function (data) {
+    const currentTime = data.seconds
+    localStorage.setItem(LOCALE_STORAGE_KEY, JSON.stringify(currentTime))
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem(LOCALE_STORAGE_KEY)
+player.setCurrentTime(savedTime)
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
 
+const LOCALE_STORAGE_KEY = "feedback-form-state"
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    textareaEl: document.querySelector('.message'),
+}
+
+
+refs.formEl.addEventListener('submit', onFormSubmit)
+refs.textareaEl.addEventListener('input', onTexareaInput)
+
+function onFormSubmit(evt) {
+    console.log('sent')
+}
+
+function onTexareaInput(evt) {
+     console.log('on input')
+}
