c23148f83bba9952d86a33edc19c2c895c9f85f2
elenashy <97391479+elenashy@users.noreply.github.com>
2022-08-11 23:21:49 +0300

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,44 @@
 
 const LOCALE_STORAGE_KEY = "feedback-form-state"
-const refs = {
-    formEl: document.querySelector('.feedback-form'),
-    textareaEl: document.querySelector('.message'),
-}
 
+const form = document.querySelector('form')
 
-refs.formEl.addEventListener('submit', onFormSubmit)
-refs.textareaEl.addEventListener('input', onTexareaInput)
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit)
+form.addEventListener('input', onFormInput)
+
+updateForm()
 
 function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    localStorage.getItem(LOCALE_STORAGE_KEY)
+
+    formData[evt.target.name] = evt.target.value;
+    console.log(formData)
+
+    evt.target.reset()
+
+    localStorage.removeItem(LOCALE_STORAGE_KEY)
+  
 }
 
-function onTexareaInput(evt) {
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+
+    localStorage.setItem(LOCALE_STORAGE_KEY, JSON.stringify(formData))
 }
+
+function updateForm() {
+    const savedMessage = localStorage.getItem(LOCALE_STORAGE_KEY)
+    const parseMessage = JSON.parse(savedMessage)
+
+    if (savedMessage) {
+        form.email.value = parseMessage.email || ""
+        form.message.value = parseMessage.message || ""
+            console.log(parseMessage)
+    }
+}
+
+
